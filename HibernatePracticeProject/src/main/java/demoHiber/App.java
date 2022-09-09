eadf0813f821681ecf0b09fe8deb3dced12d1988
PC <sachinbca00@gmail.com>
2022-09-09 09:53:58 +0530

    changes

--- HibernatePracticeProject/src/main/java/demoHiber/App.java
+++ HibernatePracticeProject/src/main/java/demoHiber/App.java
@@ -1,6 +1,8 @@
 package demoHiber;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -12,9 +14,34 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Project Started...!" );
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        
+        //#1
+//        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        
+        //#2
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
 
-
-        System.out.println(factory);
+//	CREATING SUDENT OBJECT
+        Student st = new Student();
+//  GIVING THE VALUES TO SAVE IN DATABASE      
+        st.setId(101);
+        st.setName("Sachin");
+        st.setCity("Mathura");
+        System.out.println(st);
+        
+//  GETTING THE SESSION      
+        Session session = factory.openSession();
+//  START THE TRANSACTION AND SAVE IN tx VARIABLE      
+        Transaction tx = session.beginTransaction();
+//  SAVE THE OBJECT      
+        session.save(st);
+//  THEN COMMITED THE SESSION      
+//        session.getTransaction().commit();
+        tx.commit();
+        
+        session.close();
+        
     }
 }
