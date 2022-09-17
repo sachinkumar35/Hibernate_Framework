aa9f0ff68dce8df7183f65d31fd20e9ce09abcff
PC <sachinbca00@gmail.com>
2022-09-17 21:38:00 +0530

    SQL Queries

--- HibernatePracticeProject/src/main/java/SQLQueries/SQLExample.java
+++ HibernatePracticeProject/src/main/java/SQLQueries/SQLExample.java
@@ -1,6 +1,39 @@
 package SQLQueries;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+
+import demoHiber.Student;
+
 public class SQLExample {
 
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		
+		Session s = factory.openSession();
+		
+		//SQL QUERY
+		String q = "select * from student";
+		
+		NativeQuery nq = s.createSQLQuery(q);
+		
+		List<Object[]> list = nq.list();
+		
+		for(Object[] student : list) {
+			System.out.println(Arrays.deepToString(student));
+		}
+		
+		
+		s.close();
+		factory.close();
+		
+		
+	}
 	
 }
